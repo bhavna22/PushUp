8e9deec79116f6ac983394284ad7adddf79f4fdb
unknown <bsbhavnasingh13@gmail.com>
2018-09-23 20:43:32 -0700

    Initial commit

---
+++ app/src/main/java/com/example/bsing/pushup/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.bsing.pushup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void textUpload(){
+        int i=1+1;
+    }
+}
